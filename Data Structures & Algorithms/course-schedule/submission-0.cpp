fafb172b6c18f519019d90d6d2cf5728e8ee1519
Mohtashim Khan <151078683+Mohtashimkhan22@users.noreply.github.com>
2026-07-30 10:57:47 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> inorder(n,0);
+        for(auto it : prerequisites){
+            adj[it[0]].push_back(it[1]);
+            inorder[it[1]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(inorder[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                inorder[it]--;
+                if(inorder[it]==0) q.push(it);
+            }
+        }
+        return ans.size()==n;
+    }
+};
